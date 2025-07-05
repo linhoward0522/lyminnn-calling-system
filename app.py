667bac534dc95aad425c3802ca81d01f3ed31837
linhoward0522 <linhoward71@gmail.com>
2025-07-05 19:13:54 +0800

    Update host setting

--- app.py
+++ app.py
@@ -4,18 +4,38 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-current_range = {"start": 0, "end": 0}
+# 全域變數：預設開啟查詢
+query_open = True
 
-@app.route('/status', methods=['GET'])
+# 起始與結束號碼
+start_num = 0
+end_num = 0
+
+@app.route('/')
+def home():
+    return 'Lyminnn Calling System API'
+
+@app.route('/status')
 def get_status():
-    return jsonify(current_range)
+    global query_open, start_num, end_num
+    if not query_open:
+        return jsonify({'disabled': True})
+    return jsonify({'start': start_num, 'end': end_num})
 
 @app.route('/update', methods=['POST'])
-def update_status():
+def update_range():
+    global start_num, end_num
     data = request.get_json()
-    current_range["start"] = int(data.get("start", 0))
-    current_range["end"] = int(data.get("end", 0))
-    return jsonify({"success": True, "current_range": current_range})
+    start_num = int(data.get('start', 0))
+    end_num = int(data.get('end', 0))
+    return jsonify({'success': True})
+
+@app.route('/toggle_query', methods=['POST'])
+def toggle_query():
+    global query_open
+    data = request.get_json()
+    query_open = bool(data.get('open'))
+    return jsonify({'success': True, 'query_open': query_open})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
